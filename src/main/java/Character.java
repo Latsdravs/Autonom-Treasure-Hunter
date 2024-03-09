b655417907a65153cb333abc59280844527d4853
AmiaBLe00e <alperkomurcu@outlook.com>
2024-03-09 17:58:23 +0300

    location,character

---
+++ src/main/java/Character.java
@@ -0,0 +1,20 @@
+
+public class Character {
+    int ID;
+    Location loc;
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public Character(int id, Location loc){
+        this.ID = id;
+        this.loc = loc;
+    }
+
+
+}
