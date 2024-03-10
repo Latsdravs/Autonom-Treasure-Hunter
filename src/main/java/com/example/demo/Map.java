869e088a65bd3412ac9dcaa0d99da856ecc5973f
Emin <dgbemin@gmail.com>
2024-03-11 01:31:02 +0300

    procedural generation half done.

--- src/main/java/com/example/demo/Map.java
+++ src/main/java/com/example/demo/Map.java
@@ -31,13 +31,15 @@ public class Map {
         WritableImage colorMapped = new WritableImage(offset*2+1+grid_x*(miniSize+1),offset*2+1+grid_y*(miniSize+1));
 
         PixelWriter writer = colorMapped.getPixelWriter();
-        for (int i = 0; i < 50; i++) {
-            BPSRoom(grid_x,grid_y,5,5,mapValue,542);
-        }
-        for (int i = 0; i < 4; i++) {
-            BPSRoom(grid_x,grid_y,5,15,mapValue,4);
-        }
+        BPSRoom(grid_x,grid_y,5,15,mapValue,4,20);
+
+
+
+        BPSRoom(grid_x,grid_y,4,5,mapValue,542,20);
+
+
         NoiseFill(grid_x,grid_y,65,mapValue);
+        mapValue=cellularAutomata(grid_x,grid_y,mapValue,15);
         paintCanvas(mapValue,writer,grid_x,grid_y,miniSize,offset);
 
 
@@ -229,48 +231,113 @@ public class Map {
         }
 
     }
-    void cellularAutomata( int grid_x,int grid_y,int darkID){
+    int[][] cellularAutomata( int grid_x,int grid_y,int[][] map,int iteration){
+        int [][] phase1 = new int[map.length][];
+        int [][] phase2 = new int[map.length][];
+        for(int i = 0; i < map.length; i++) {
+            phase1[i] = map[i].clone();
+            phase2[i] = map[i].clone();
+        }
+        for (int r = 0; r < iteration; r++) {
+            int[][] temp = phase1;
+            phase1 = phase2;
+            phase2 = temp;
+            for (int i = 0; i < grid_x; i++) {
+                for (int j = 0; j < grid_y; j++) {
+                    if(map[i][j]!=0 && map[i][j]!=70153)continue;
+                    int darkCount=0;
+                    if(i > 0){
+                        if(phase1[i-1][j]!=0)darkCount++;
+
+                        if(j > 0 && phase1[i-1][j-1]!=0)darkCount++;
+
+
+                    }
+                    if(j > 0){
+                        if(phase1[i][j-1]!=0)darkCount++;
+
+                        if(i < grid_x-1 && phase1[i+1][j-1]!=0)darkCount++;
+
+
+                    }
+                    if(i < grid_x-1){
+                        if(phase1[i+1][j]!=0)darkCount++;
+
+                        if(j < grid_y-1 && phase1[i+1][j+1]!=0)darkCount++;
+
+
+                    }
+                    if(j < grid_y-1){
+                        if(phase1[i][j+1]!=0)darkCount++;
+
+                        if(i > 0 && phase1[i-1][j+1]!=0)darkCount++;
+
+
+                    }
+                    if(darkCount > 4)phase2[i][j]=70153;
+                    else phase2[i][j]=0;
+
+                }
+            }
+        }
+        return phase2;
+
 
     }
-    void BPSRoom(int grid_x,int grid_y,int expSpace,int roomSize,int[][] map,int ID) {
-        BPSRoom(grid_x,grid_y,expSpace,roomSize,roomSize,map,ID);
+    void BPSRoom(int grid_x,int grid_y,int expSpace,int roomSize,int[][] map,int ID,int iteration) {
+        BPSRoom(grid_x,grid_y,expSpace,roomSize,roomSize,map,ID,iteration);
     }
 
-    void BPSRoom(int grid_x,int grid_y,int expSpace,int roomSize_x,int roomSize_y,int[][] map,int ID){
-        Random random = new Random();
-        int steps_x = Integer.highestOneBit(grid_x)>>expSpace;
-        int steps_y = Integer.highestOneBit(grid_y)>>expSpace;
-        int theStep_x = random.nextInt(steps_x);
-        int theStep_y = random.nextInt(steps_y);
-        int start_x = 0;
-        int end_x = grid_x;
-
-        int start_y = 0;
-        int end_y = grid_y;
-        for (int i = 1; i < steps_x; i*=2) {
-
-            if(theStep_x%2==1)
-                start_x=(end_x+start_x)/2;
-            else
-                end_x = (end_x+start_x)/2;
-            theStep_x/=2;
-        }
-        for (int i = 1; i < steps_y; i*=2) {
-
-            if(theStep_y%2==1)
-                start_y=(end_y+start_y)/2;
-            else
-                end_y = (end_y+start_y)/2;
-            theStep_y/=2;
-        }
-        //random sayı 11111 iken dene
+    void BPSRoom(int grid_x,int grid_y,int expSpace,int roomSize_x,int roomSize_y,int[][] map,int ID,int iteration){
         int[] empties = new int[]{0};
 
-        if(checkEdge(map,start_x,end_x,start_y,end_y,empties)){
-            int temp_x = random.nextInt(start_x,end_x-roomSize_x+1);
-            int temp_y = random.nextInt(start_y,end_y-roomSize_y+1);
-            fillMatrix(map,temp_x,temp_x+roomSize_x,temp_y,temp_y+roomSize_y,ID);
-        }else BPSRoom(grid_x,grid_y,expSpace,roomSize_x,roomSize_y,map,ID);
+        int steps_x = Integer.highestOneBit(grid_x)>>expSpace;
+        int steps_y = Integer.highestOneBit(grid_y)>>expSpace;
+        int[] locations = new int[iteration];
+        for (int r = 0; r < iteration; r++) {
+            Random random = new Random();
+            int theStep_x = random.nextInt(steps_x);
+            int theStep_y = random.nextInt(steps_y);
+            int start_x = 0;
+            int end_x = grid_x;
+
+            int start_y = 0;
+            int end_y = grid_y;
+            int theLocation=0;
+            for (int i = 1; i < steps_x; i*=2) {
+                theLocation<<=1;
+
+                if(theStep_x%2==1) {
+                    start_x = (end_x + start_x) / 2;
+                    theLocation++;
+                }
+                else
+                    end_x = (end_x+start_x)/2;
+
+                theStep_x/=2;
+            }
+            for (int i = 1; i < steps_y; i*=2) {
+                theLocation<<=1;
+                if(theStep_y%2==1) {
+                    start_y = (end_y + start_y) / 2;
+                    theLocation++;
+                }
+                else
+                    end_y = (end_y+start_y)/2;
+
+                theStep_y/=2;
+            }
+            //random sayı 11111 iken dene
+
+
+            if(checkSpace(map,start_x,end_x,start_y,end_y,empties)){
+                int temp_x = random.nextInt(start_x,end_x-roomSize_x+1);
+                int temp_y = random.nextInt(start_y,end_y-roomSize_y+1);
+                fillMatrix(map,temp_x,temp_x+roomSize_x,temp_y,temp_y+roomSize_y,ID);
+                locations[r]=theLocation;
+            }else r--;
+        }
+
 
 
 
